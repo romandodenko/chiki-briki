d2a401183806663dffb1bdeac703ff0be6044254
Roman Dodenko <creed6695@mail.ru>
2023-02-01 19:03:33 +0700

    add popup

--- js/main.js
+++ js/main.js
@@ -2,6 +2,35 @@
 
 window.onload = function () {
 
+
+  document.addEventListener("click", function (e) {
+    const elementInteractive = e.target;
+
+    if (elementInteractive.closest(".popup__btn")) {
+      document.querySelector(".popup").classList.remove("popup-active")
+    }
+    if (!elementInteractive.closest(".popup__body")) {
+      document.querySelector(".popup").classList.remove("popup-active")
+    }
+  })
+
+  const popup = document.querySelector(".popup");
+
+  if (popup) {
+    let popupTimer, timeOut = 4000;
+
+    function displayPopup() {
+      popup.classList.add("popup-active")
+    }
+
+    popupTimer = setTimeout(displayPopup, timeOut);
+
+    window.addEventListener("scroll", function (e) {
+      clearTimeout(popupTimer);
+      popupTimer = setTimeout(displayPopup, timeOut);
+    })
+  };
+
   const headerSliderInit = document.querySelector(".header-slider");
 
   if (headerSliderInit) {
